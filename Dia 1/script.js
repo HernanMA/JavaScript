86d0ecd6a87982ca032a88c8d1d0cfad41ee33ed
Hernan <hernanariza69@gmail.com>
2024-04-11 08:55:42 -0500

    feat: :sparkles:

--- Dia 1/script.js
+++ Dia 1/script.js
@@ -7,3 +7,41 @@ let palabra1 = "Campus";
 let palabra2 = "Lands";
 concatenacion = palabra1 + palabra2;
 console.log(concatenacion);
+
+
+// ***************** FUNCIONES *****************
+
+
+// ***************** FUNCION SIN RETORNO Y SIN PARAMETROS *****************
+function funcionNormal(){
+    console.log("Mi función");
+}
+
+// ***************** FUNCION SIN RETORNO Y CON PARAMETROS *****************
+function suma(a,b){
+    console.log(a+b);
+}
+// ***************** FUNCION CON RETORNO Y CON PARAMETROS *****************
+function sumaR(a,b){
+    //console.log(a+b);
+    return a+b;
+}
+// ***************** FUNCION CON RETORNO Y SIN PARAMETROS *****************
+function salonFavorito(){
+    //console.log(a+b);
+    return "P1";
+}
+
+
+// ***************** BUCLE FOR *****************
+
+for (let i=0; i<5; i= i+1){
+    console.log(i);   
+}
+
+
+
+// Actividad 1
+let celcius = prompt("Por favor ingrese los grados centigrados");
+fahrenheit = 32 + (9 * celcius / 5);
+alert(fahrenheit);
