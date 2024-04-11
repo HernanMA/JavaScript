8f31d72d37c4ab23a8782105b7e860daca7b0f7c
Hernan <hernanariza69@gmail.com>
2024-04-11 08:04:12 -0500

    feat: :sparkles: Script working

---
+++ Dia 1/script.js
@@ -0,0 +1,9 @@
+let a = 10;
+let b = 2;
+console.log(a*b);
+console.log(typeof a)
+
+let palabra1 = "Campus";
+let palabra2 = "Lands";
+concatenacion = palabra1 + palabra2;
+console.log(concatenacion);
