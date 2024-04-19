e78a3a69fce8ebe79929a91afff9bfb13b3f41b4
Hernan <hernanariza69@gmail.com>
2024-04-19 09:00:20 -0500

    feat: :sparkles: Exercice 4 done

--- Dia 6/script.js
+++ Dia 6/script.js
@@ -88,3 +88,41 @@ const myCar = new Car ('Toyota', 'Ford', 2015, 4);
 
 //Display details of the car
 myCar.displayDetailss();
+
+
+
+//Exercice 4
+class BankAccount {
+    constructor(accountNumber, balance) {
+        this.accountNumber = accountNumber;
+        this.balance = balance;
+    }
+
+    //Method to deposit money into the account
+    deposit(amount) {
+        this.balance += amount;
+        console.log(`Deposited ${amount} into account ${this.accountNumber} New balance: ${this.balance}`);
+    }
+
+    //Method to withdraw money from the account
+    withdraw(amount) {
+        if (amount <= this.balance) {
+            this.balance -= amount;
+            console.log(`withdrawn ${amount}, from account ${this.accountNumber}. New balance: ${this.balance}`);
+        } else {
+            console.log(`Insufficient funds. Cannot withdraw ${amount} from account ${this.accountNumber}`)
+        }
+    }
+}
+
+//Create instances of the BankAccount class
+const account1 = new BankAccount('111111', 1000)
+const account2 = new BankAccount('222222', 500)
+
+//Deposit money into accounts
+account1.deposit(200);
+account2.deposit(300);
+
+//withdraw money from accounts
+account1.withdraw(300);
+account2.withdraw(600);
