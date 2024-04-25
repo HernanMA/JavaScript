f9d1c13ad7fcee322b74304fd63e9cf485ae9296
Hernan <hernanariza69@gmail.com>
2024-04-24 22:22:49 -0500

    feat: :sparkles: Quiz corregido

--- Dia 8(Quiz)/script.js
+++ Dia 8(Quiz)/script.js
@@ -1,86 +1,130 @@
-const APP = {
-    urls: {
-        base: 'https://swapi.py4e.com/api/',
-        people: 'people/',
-        planets: 'planets/',
-        films: 'films/',
-        species: 'species/',
-        vehicles: 'vehicles/',
-        starships: 'starships/',
-    },
-    init: () => {
-        APP.addListeners();
-        APP.buildNav();
-    },
-    addListeners: () => {
-        let nav = document.getElementById('nav');
-        nav.addEventListener('click', APP.getData);
-        footer.addEventListener('click', APP.getData);
-    },
-    buildNav: () => {
-        let df = new DocumentFragment();
-        for (let nm in APP.urls) {
-            if (nm != 'base') {
-                let link = document.createElement('a');
-                link.href = `${APP.urls.base}${APP.urls[nm]}`;
-                link.textContent = nm;
-                link.setAttribute('data-link', `${APP.urls.base}${APP.urls[nm]}`);
-                df.append(link);
-            }
+function fetchSuperHero() {
+    let xhr = new XMLHttpRequest();
+    let heroID = document.getElementById('Id').value;
+    console.log(heroID);
+    let url = `https://swapi.py4e.com/api/people/${heroID}/`;
+    xhr.open('GET', url, true);
+    xhr.onreadystatechange = function () {
+        if (this.readyState === 4 && this.status === 200) {
+            let persona = JSON.parse(this.responseText);
+            console.log(persona);
+            displayHero(persona);
         }
-        document.getElementById('nav').append(df);
-    },
-    getData: (ev) => {
-        if (ev) ev.preventDefault();
-        document.querySelector('.overlay').classList.add('active');
-        let link = ev.target;
-        let url = link.getAttribute('data-link');
-        fetch(url)
-            .then((resp) => {
-                if (!resp.ok) throw new Error(resp.statusText);
-                return resp.json();
-            })
-            .then(APP.buildList)
-            .catch((err) => {
-                console.error(err);
-                document.querySelector('.overlay').classList.remove('active');
-            });
-    },
-    buildList: (data) => {
-        let m = document.getElementById('main');
-        console.log(data);
-
-        document.querySelector('.overlay').classList.remove('active');
-        m.innerHTML = data.results
-            .map((item) => {
-                let nm = item.name || item.title;
-                return `<p>${nm}</p>`;
-            })
-            .join(' ');
-        let footer = document.getElementById('footer');
-        footer.innerHTML = '';
-
-        if (data.previous) {
-            let prev = document.createElement('a');
-            prev.href = data.previous;
-            let url = new URL(data.previous);
-            let labels = url.pathname.split('/');
-            let label = labels[labels.length - 2];
-            prev.textContent = `<< Previous ${label}`;
-            prev.setAttribute('data-link', data.previous);
-            footer.append(prev);
+        else if (this.readyState == 4) {
+            console.log('Error:', this.statusText);
         }
-        if (data.next) {
-            let next = document.createElement('a');
-            next.href = data.next;
-            let url = new URL(data.next);
-            let labels = url.pathname.split('/');
-            let label = labels[labels.length - 2];
-            next.textContent = `Next ${label} >>`;
-            next.setAttribute('data-link', data.next);
-            footer.append(next);
-        }
-    },
-};
+    };
+    xhr.send();
+}
 
-document.addEventListener('DOMContentLoaded', APP.init);
+function displayHero(persona) {
+    let heroInfo = document.getElementById("superHeroInfo");
+    if (persona.detail) {
+        heroInfo.innerHTML = `<p>Error: ${persona.detail}</p>`;
+    } else {
+        heroInfo.innerHTML = `
+        <h1>Persona</h1>
+        <p>Nombre: ${persona.name}</p>
+        <p>Height: ${persona.height}</p>
+        <p>Mass: ${persona.mass}</p>
+        <p>hair_color: ${persona.hair_color}</p>
+        <p>skin_color: ${persona.skin_color}</p>
+        <p>eye_color: ${persona.eye_color}</p>
+        <p>birth_year: ${persona.birth_year}</p>
+        <p>gender: ${persona.gender}</p>
+        <p>homeworld: ${persona.homeworld}</p>
+        <p>films: ${persona.films}</p>
+        <p>species: ${persona.species}</p>
+        <p>vehicles: ${persona.vehicles}</p>
+        <p>starships: ${persona.starships}</p>
+        <p>created: ${persona.created}</p>
+        <p>edited: ${persona.edited}</p>
+        <p>url: ${persona.url}</p>
+        `;
+        fetchSpecies(persona.species);
+        fetchWord(persona.homeworld);
+    }
+}
+
+function fetchSpecies(Specie) {
+    let xhr = new XMLHttpRequest();
+    console.log(Specie);
+    xhr.open('GET', Specie, true);
+    xhr.onreadystatechange = function () {
+        if (this.readyState === 4 && this.status === 200) {
+            let Species = JSON.parse(this.responseText);
+            console.log(Species);
+            displayEspecie(Species);
+        }
+        else if (this.readyState == 4) {
+            console.log('Error:', this.statusText);
+        }
+    };
+    xhr.send();
+}
+
+function displayEspecie(Specie) {
+    let SpecieInfo = document.getElementById("SpecieInfo");
+    if (Specie.detail) {
+        SpecieInfo.innerHTML = `<p>Error: ${Specie.detail}</p>`;
+    } else {
+        SpecieInfo.innerHTML = `
+        <h1>Especie persona</h1>
+        <p>Nombre: ${Specie.name}</p>
+        <p>classification: ${Specie.classification}</p>
+        <p>designation: ${Specie.designation}</p>
+        <p>average_height: ${Specie.average_height}</p>
+        <p>skin_colors: ${Specie.skin_colors}</p>
+        <p>hair_colors: ${Specie.hair_colors}</p>
+        <p>eye_colors: ${Specie.eye_colors}</p>
+        <p>average_lifespan: ${Specie.average_lifespan}</p>
+        <p>language: ${Specie.language}</p>
+        <p>created: ${Specie.created}</p>
+        <p>edited: ${Specie.edited}</p>
+        <p>url: ${Specie.url}</p>
+
+        
+        `;
+    }
+}
+
+function fetchWord(mundo) {
+    let xhr = new XMLHttpRequest();
+    console.log(mundo);
+    xhr.open('GET', mundo, true);
+    xhr.onreadystatechange = function () {
+        if (this.readyState === 4 && this.status === 200) {
+            let word = JSON.parse(this.responseText);
+            console.log(word);
+            displayMundo(word);
+        }
+        else if (this.readyState == 4) {
+            console.log('Error:', this.statusText);
+        }
+    };
+    xhr.send();
+}
+
+function displayMundo(mundo) {
+    let mundoInfo = document.getElementById("mundoInfo");
+    if (mundo.detail) {
+        mundoInfo.innerHTML = `<p>Error: ${mundo.detail}</p>`;
+    } else {
+        mundoInfo.innerHTML = `
+        <h1>Mundo persona</h1>
+        <p>Nombre: ${mundo.name}</p>
+        <p>Rotation Period: ${mundo.rotation_period}</p>
+        <p>Orbital Period: ${mundo.orbital_period}</p>
+        <p>Diameter: ${mundo.diameter}</p>
+        <p>Climate: ${mundo.climate}</p>
+        <p>Gravity: ${mundo.gravity}</p>
+        <p>Terrain: ${mundo.terrain}</p>
+        <p>Surface Water: ${mundo.surface_water}</p>
+        <p>Population: ${mundo.population}</p>
+        <p>Films: ${mundo.films}</p>
+        <p>Created: ${mundo.created}</p>
+        <p>Edited: ${mundo.edited}</p>
+        <p>URL: ${mundo.url}</p>
+        `;
+    }
+}
