577987ecc6618114e0aa61ad3c4b41a5672a94ae
Hernan <hernanariza69@gmail.com>
2024-05-03 09:07:41 -0500

    feat: :sparkles:

---
+++ Dia 13/script.js
@@ -0,0 +1,34 @@
+let Heroes = [];
+
+function init(){
+    loadHeroes();
+}
+
+function loadHeroes(){
+    let xhttp = new XMLHttpRequest();
+
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            let responseObject = JSON.parse(xhttp.responseText);
+            Heroes = responseObject;
+            render();
+        }
+    };
+
+    xhttp.open("GET", "data.json", true);
+    xhttp.send();
+}
+
+function render(){
+    let content = "";
+    Heroes.forEach(heroe =>{
+        content += `
+            <div>
+                <p> Heroe: ${heroe.superhero}</p>
+                <br/>
+                <p> Universo: ${heroe.publisher}</p>
+            </div>
+        `
+    })
+    document.getElementById("Heroediv").innerHTML = content;
+}
