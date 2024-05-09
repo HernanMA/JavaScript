be865ee3b156c8888425b45645ef02ce254b9bab
Hernan <hernanariza69@gmail.com>
2024-05-09 08:44:24 -0500

    feat: :sparkles: done

--- Dia 17/script.js
+++ Dia 17/script.js
@@ -1,49 +1,50 @@
-const container = document.getElementById('container');
-const shadowRoot = container.attachShadow({ mode: 'open' });
+class SoyPro extends HTMLElement{
+    constructor(){
+        super();
+        this.render();
+    }
+    render(){
+        this.innerHTML = `
+        <div class="container">
+        <img id="foto" src="" alt="imagen">
+        <h2>My name is <br> <span id="nombre"></span></h2>
 
-const div = document.createElement('div');
+        <div class="datos">
+            <p>Correo: <span id="correo"></span></p>
+            <p>Celular: <span id="telefono"></span></p>
+            <p>Cumpleaños: <span id="Cumpleanos"></span></p>
+            <p>Ubicación: <span id="ubicacion"></span></p>
+            <p>Contraseña: <span id="password"></span></p>
+        </div>
 
-div.classList.add('container');
+        <div id="infoHover"></div>
 
-shadowRoot.appendChild(div);
+        <div class="iconos">
+            <div class="gg-user"></div>
+            <div class="gg-voicemail-r"></div>  
+            <div class="gg-calendar-dates"></div>
+            <div class="gg-flag"></div>
+            <div class="gg-smartphone-shake"></div>
+            <div class="gg-block"></div>
+        </div>
+    </div>
+        `;
+    }
+}
 
-div.innerHTML = `
-<div class="container">
-<img id="foto" src="" alt="imagen">
-<h2>My name is <br> <span id="nombre"></span></h2>
+customElements.define('soy-pro',SoyPro);
 
-<div class="datos">
-    <p>Correo: <span id="correo"></span></p>
-    <p>Celular: <span id="telefono"></span></p>
-    <p>Cumpleaños: <span id="Cumpleanos"></span></p>
-    <p>Ubicación: <span id="ubicacion"></span></p>
-    <p>Contraseña: <span id="password"></span></p>
-</div>
 
-<div id="infoHover"></div>
 
-<div class="iconos">
-    <div class="gg-user"></div>
-    <div class="gg-voicemail-r"></div>  
-    <div class="gg-calendar-dates"></div>
-    <div class="gg-flag"></div>
-    <div class="gg-smartphone-shake"></div>
-    <div class="gg-block"></div>
-</div>
-</div>
-`;
-
-const nombre = shadowRoot.getElementById('nombre');
-const correo = shadowRoot.getElementById('correo');
-const telefono = shadowRoot.getElementById('telefono');
-const foto = shadowRoot.getElementById('foto');
-const Cumpleanos = shadowRoot.getElementById('Cumpleanos');
-const ubicacion = shadowRoot.getElementById('ubicacion');
-const password = shadowRoot.getElementById('password');
-const iconos = shadowRoot.querySelectorAll('.iconos div');
-const infoHover = shadowRoot.getElementById('infoHover');
-
-let datoActual = null;
+const nombre = document.querySelector('#nombre');
+const correo = document.querySelector('#correo');
+const telefono = document.querySelector('#telefono');
+const foto = document.querySelector('#foto');
+const Cumpleanos = document.querySelector('#Cumpleanos');
+const ubicacion = document.querySelector('#ubicacion');
+const password = document.querySelector('#password');
+const iconos = document.querySelectorAll('.iconos div');
+const infoHover = document.querySelector('#infoHover');
 
 const generarUsuario = async () => {
     const url = 'https://randomuser.me/api/';
