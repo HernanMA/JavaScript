1f04445fb58c9493bc4aaf53fc4fad82cf1b72b7
Hernan <hernanariza69@gmail.com>
2024-05-10 07:25:43 -0500

    feat: :memo: Documentation done

---
+++ Correciones/Dia 16/script.js
@@ -0,0 +1,64 @@
+const nombre = document.querySelector('#nombre');
+const correo = document.querySelector('#correo');
+const telefono = document.querySelector('#telefono');
+const foto = document.querySelector('#foto');
+const Cumpleanos = document.querySelector('#Cumpleanos');
+const ubicacion = document.querySelector('#ubicacion');
+const password = document.querySelector('#password');
+const iconos = document.querySelectorAll('.iconos div');
+const infoHover = document.querySelector('#infoHover');
+
+const generarUsuario = async () => {
+    const url = 'https://randomuser.me/api/';
+    const respuesta = await fetch(url);
+    const { results } = await respuesta.json();
+    const datos = results[0];
+
+    foto.src = datos.picture.medium;
+    nombre.textContent = datos.name.first;
+    correo.textContent = datos.email;
+    telefono.textContent = datos.phone;
+    Cumpleanos.textContent = datos.dob.date;
+    ubicacion.textContent = datos.location.city;
+    password.textContent = datos.login.password;
+}
+
+const mostrarDato = (dato, textoAdicional) => {
+    if (dato !== datoActual) {
+        infoHover.innerHTML = `${textoAdicional}:<br>${dato}`;
+        datoActual = dato;
+    }
+}
+const limpiarInfo = () => {
+    infoHover.textContent = '';
+    datoActual = null;
+}
+
+generarUsuario();
+
+iconos.forEach((icono, index) => {
+    icono.addEventListener('mouseover', () => {
+        switch (index) {
+            case 0:
+                mostrarDato(nombre.textContent, 'Hola, Mi nombre es');
+                break;
+            case 1:
+                mostrarDato(correo.textContent, 'Mi correo es');
+                break;
+            case 2:
+                mostrarDato(telefono.textContent, 'Mi número de telefono es');
+                break;
+            case 3:
+                mostrarDato(Cumpleanos.textContent, 'Mi fecha de cumpleaños es');
+                break;
+            case 4:
+                mostrarDato(ubicacion.textContent, 'Yo me ubico en');
+                break;
+            case 5:
+                mostrarDato(password.textContent, 'Mi contraseña es');
+                break;
+        }
+    });
+
+    icono.addEventListener('mouseout', limpiarInfo);
+});
---
+++ Dia 14/script.js
@@ -0,0 +1,98 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const btnMostrar = document.getElementById("Mostrar");
+    const btnCrear = document.getElementById("Crear");
+    const bodyDetail = document.querySelector(".body-detail");
+    let heroes = []; // JSON de ejemplo
+
+    // Función para mostrar los héroes cargados desde el archivo JSON
+    function mostrarHeroesDesdeJSON() {
+        fetch("data.json")
+            .then(response => response.json())
+            .then(data => {
+                bodyDetail.innerHTML = "";
+                data.forEach(heroe => {
+                    const heroDiv = document.createElement("div");
+                    heroDiv.textContent = `${heroe.superhero} - ${heroe.publisher} - ${heroe.alter_ego} - ${heroe.first_appearance} - ${heroe.characters}`;
+                    bodyDetail.appendChild(heroDiv);
+                });
+            })
+            .catch(error => {
+                console.error("Error al cargar el archivo JSON:", error);
+            });
+    }
+
+// Evento para eliminar el último héroe añadido cuando se hace clic en el botón "Eliminar"
+const btnEliminar = document.getElementById("Eliminar");
+btnEliminar.addEventListener("click", function() {
+    if(heroes.length > 0) {
+        heroes.pop(); // Elimina el último héroe del array
+        console.log("Héroe eliminado, héroes restantes:", heroes);
+
+        // Opcional: Actualizar la interfaz para reflejar el héroe eliminado
+        const heroesDivs = bodyDetail.querySelectorAll("div");
+        if(heroesDivs.length > 0) {
+            bodyDetail.removeChild(heroesDivs[heroesDivs.length - 1]);
+        }
+    } else {
+        console.log("No hay héroes para eliminar");
+    }
+});
+
+    // Evento para actualizar los datos en el servidor cuando se hace clic en el botón "Actualizar"
+    const btnActualizar = document.getElementById("Actualizar");
+    btnActualizar.addEventListener("click", function () {
+        fetch('data.json', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(heroes) // Envía el array de héroes como JSON
+        })
+            .then(response => response.json())
+            .then(data => {
+                console.log('Datos guardados:', data);
+            })
+            .catch((error) => {
+                console.error('Error:', error);
+            });
+    });
+
+
+    // Evento para mostrar los héroes cuando se hace clic en el botón "Ver Heroes"
+    btnMostrar.addEventListener("click", mostrarHeroesDesdeJSON);
+
+    // Evento para agregar un nuevo héroe cuando se hace clic en el botón "Nuevo Heroe"
+    btnCrear.addEventListener("click", function() {
+        const characterName = document.getElementById("characterName").value;
+        const actorName = document.getElementById("actorName").value;
+        const age = document.getElementById("age").value;
+        const cityName = document.getElementById("cityName").value;
+        const poster = document.getElementById("poster").value;
+        const dateAppears = document.getElementById("dateAppears").value;
+        const producer = document.getElementById("producer").value;
+        const suits = document.querySelectorAll(".suit");
+
+        let suitsArray = [];
+        suits.forEach(suit => {
+            if(suit.value !== "") { // Asegurarse de que el valor no esté vacío
+                suitsArray.push(suit.value);
+            }
+        });
+
+        const nuevoHeroe = {
+            superhero: characterName,
+            publisher: producer,
+            alter_ego: actorName,
+            first_appearance: dateAppears,
+            characters: suitsArray.join(", ")
+        };
+
+        heroes.push(nuevoHeroe);
+        console.log("Nuevo héroe agregado:", nuevoHeroe);
+
+        // Opcional: Actualizar la interfaz con el nuevo héroe
+        const heroDiv = document.createElement("div");
+        heroDiv.textContent = `${nuevoHeroe.superhero} - ${nuevoHeroe.publisher} - ${nuevoHeroe.alter_ego} - ${nuevoHeroe.first_appearance} - ${nuevoHeroe.characters}`;
+        bodyDetail.appendChild(heroDiv);
+    });
+});
